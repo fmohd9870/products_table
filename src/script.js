88eeefa7679d7887cf900a0c0d37b34e31f010ec
Mohd <mohdfaiz1848816@gmail.com>
2022-02-08 18:01:47 +0530

    add and edit functionality

--- src/script.js
+++ src/script.js
@@ -22,8 +22,12 @@ function fun() {
   var id = document.getElementById("id").value;
   var name = document.getElementById("name").value;
   var price = document.getElementById("price").value;
-  var b = { id: id, name: name, price: price };
-  a.push(b);
+  if (id == "" || name == "" || price == "") {
+    alert("please insert all the values");
+  } else {
+    var b = { id: id, name: name, price: price , };
+    a.push(b);
+  }
 
   // document.getElementById("table_body").innerHTML="";
   display();
@@ -48,11 +52,15 @@ function display() {
         a[i].name +
         '",' +
         a[i].price +
-        ")'><input type='button' Value='Delete' onclick='delete()'></td></tr>";
+        ")'><input type='button' Value='Delete' onclick='del(" +
+        a[i].id +
+        ")'></td></tr>";
       document.getElementById("id").value = "";
       document.getElementById("name").value = "";
       document.getElementById("price").value = "";
     }
+    console.log(a);
+    // console.log(temp);
     document.getElementById("table_body").innerHTML = temp;
   }
 }
@@ -62,4 +70,40 @@ function edit(pid, pname, pprice) {
   document.getElementById("price").value = pprice;
   document.getElementById("add").style.display = "none";
   document.getElementById("edit").style.display = "block";
+
+  // display();
+  // update(pid, pname, price);
+}
+function update(pid, pname, pprice) {
+  for (var i = 0; i <a.length; i++) {
+    if (a[i].id == document.getElementById("id").value) {
+      console.log(a[i].name);
+      a[i].name = document.getElementById("name").value;
+      console.log(a[i].name);
+      a[i].price = document.getElementById("price").value;
+      console.log(a[i].price);
+    }
+    
+  }
+  document.getElementById("add").style.display = "block";
+  document.getElementById("edit").style.display = "none";
+  display();
+    console.log("end for");
+
+  
+}
+function del(pid) {
+  // index1=a.indexOf(pid);
+  // console.log(index1);
+  arr = [];
+  for (var i = 0; i < a.length; i++) {
+    if (a[i].id == pid) {
+      a.splice(i, 1);
+      // delete a[0];
+      console.log("element deleted");
+      console.log(a.length);
+      // console.log(a);
+    }
+  }
+  display();
 }
